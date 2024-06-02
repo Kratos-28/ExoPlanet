c92342d5670e1f841cd59ff32ff2eae8b1930778
suraj-afk <92143002+S-uraj@users.noreply.github.com>
2024-06-02 17:35:05 +0530

    Extend Exoplanet Type

--- models/exoplanet.go
+++ models/exoplanet.go
@@ -5,14 +5,21 @@ import (
 	"sync"
 )
 
+type ExoPlanetType string
+
+const (
+	GasGiant   ExoPlanetType = "GasGiant"
+	Terrestial ExoPlanetType = "Terrestial"
+)
+
 type Exoplanet struct {
-	ID          string   `json:"id"`
-	Name        string   `json:"name"`
-	Description string   `json:"description"`
-	Distance    float64  `json:"distance"`
-	Radius      float64  `json:"radius"`
-	Type        string   `json:"type"`
-	Mass        *float64 `json:"mass,omitempty"`
+	ID          string        `json:"id"`
+	Name        string        `json:"name"`
+	Description string        `json:"description"`
+	Distance    float64       `json:"distance"`
+	Radius      float64       `json:"radius"`
+	Type        ExoPlanetType `json:"type"`
+	Mass        *float64      `json:"mass,omitempty"`
 }
 
 type ExoplanetStore struct {
